a12569618090153e96f0ee36980d0f72f291c465
moshe-azgari <mosheazgari@gmail.com>
2016-05-24 11:26:25 +0300

    entity data models

---
+++ EasyDbProject/DbProject/Model/job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbProject.Model
+{
+    public class job
+    {
+        public int jobId { set; get; }
+        public string jobDescription { set; get; }
+        public virtual List<worker> workers { set; get; }
+    }
+}
---
+++ EasyDbProject/DbProject/Model/worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbProject.Model
+{
+    public class worker
+    {
+        public int idNumber { set; get; }
+        public string firstName { set; get; }
+        public string lastName { set; get; }
+        public DateTime dob { set; get; }
+        public string age { get { int me = DateTime.Now.Year - dob.Year; return me.ToString();} }
+
+        public int jobId { set; get; }
+        public virtual job job { set; get; }
+    }
+}
